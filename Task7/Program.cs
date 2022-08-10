27a3b2ed82590553df8f6aafae3da50638be4641
Masterov_IN <masterov_in@mail.ru>
2022-08-10 11:30:45 +0300

    Task7 done

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дополнительные задачи (не обязательные)
+// Задача 7: Напишите программу, 
+// которая принимает на вход трёхзначное 
+// число и на выходе показывает последнюю 
+// цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int lastnum = number % 10;
+
+Console.WriteLine($"Последняя цифра числа {number}: {lastnum}");
